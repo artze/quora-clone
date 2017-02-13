cbbacd4018edc012ed10ad901548e33e0c832351
artze <arthurwtm@gmail.com>
2017-02-13 18:18:38 +0800

    migrated user foreign key to questions and answers tables

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,5 @@
 class Question < ActiveRecord::Base
 	# This is Sinatra! Remember to create a migration!
 	has_many :answers
+	belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
 	# This is Sinatra! Remember to create a migration!
 	has_secure_password
+	has_many :questions
+	has_many :answers
 	validates :email, :username, presence: true
 	validates :email, :username, uniqueness: true
 end
